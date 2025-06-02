36064fb3b62887bb4546c71029235bea606d79ca
yuto-matsuda <yutochin.dev@gmail.com>
2025-06-03 05:54:21 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/certificate-exam-tracer/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     react(),
     tailwindcss(),
